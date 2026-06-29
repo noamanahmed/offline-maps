4db5b5c38125159c69f50b639b9061bca52b9044
Noaman Ahmed <noamanahmd@gmail.com>
2026-06-30 02:30:37 +0500

    Fix: Fixed CI

--- flutter_app/lib/services/file_loader_stub.dart
+++ flutter_app/lib/services/file_loader_stub.dart
@@ -1,12 +1,28 @@
 import 'dart:convert';
 import 'dart:typed_data';
+import 'package:flutter/services.dart' show rootBundle;
 
 class FileLoader {
   FileLoader._();
   static final _i = FileLoader._();
   factory FileLoader() => _i;
 
-  Future<String?> readText(String path) async => null;
-  Future<Uint8List?> readBinary(String path) async => null;
+  Future<String?> readText(String path) async {
+    try {
+      return await rootBundle.loadString('web/$path');
+    } catch (_) {
+      return null;
+    }
+  }
+
+  Future<Uint8List?> readBinary(String path) async {
+    try {
+      final data = await rootBundle.load('web/$path');
+      return data.buffer.asUint8List();
+    } catch (_) {
+      return null;
+    }
+  }
+
   String toBase64(Uint8List bytes) => base64Encode(bytes);
 }
--- flutter_app/lib/widgets/map_widget_stub.dart
+++ flutter_app/lib/widgets/map_widget_stub.dart
@@ -1,25 +1,79 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MapController {
-  void callMapFn(String fn, [List<dynamic> args = const []]) {}
-  void loadMapData(String pbfB64, String poisJson, double lat, double lon, int zoom) {}
-  void setTheme(String theme) {}
-  void updateUserLocation(double lat, double lng, bool connected) {}
-  void centerOnUser() {}
-  void centerOnCoords(double lat, double lng, [int? z]) {}
-  void focusPoi(double lat, double lng, String name) {}
-  void drawRoute(double fl, double fg, double tl, double tg) {}
-  void clearRoute() {}
-  void clearSelectedPin() {}
-  void runSearch(String q, double al, double ag) {}
+  WebViewController? _controller;
+
+  Future<void> _init(WebViewController c) async {
+    _controller = c;
+  }
+
+  void callMapFn(String fn, [List<dynamic> args = const []]) {
+    final argsJson = args.map((a) => a is String ? '"${a.replaceAll('"', '\\"')}"' : '$a').join(',');
+    _controller?.runJavaScript('if(window.$fn) window.$fn($argsJson)');
+  }
+
+  void loadMapData(String pbfB64, String poisJson, double lat, double lon, int zoom) =>
+      callMapFn('loadMapData', [pbfB64, poisJson, lat, lon, zoom, false]);
+  void setTheme(String t) => callMapFn('setTheme', [t]);
+  void updateUserLocation(double lat, double lng, bool c) =>
+      callMapFn('updateUserLocation', [lat, lng, c]);
+  void centerOnUser() => callMapFn('centerOnUser');
+  void centerOnCoords(double lat, double lng, [int? z]) =>
+      callMapFn('centerOnCoords', [lat, lng, z]);
+  void focusPoi(double lat, double lng, String name) =>
+      callMapFn('focusPoi', [lat, lng, name]);
+  void drawRoute(double fl, double fg, double tl, double tg) =>
+      callMapFn('drawRoute', [fl, fg, tl, tg]);
+  void clearRoute() => callMapFn('clearRoute');
+  void clearSelectedPin() => callMapFn('clearSelPin');
+  void runSearch(String q, double al, double ag) =>
+      callMapFn('runSearch', [q, al, ag]);
 }
 
-class MapWidget extends StatelessWidget {
+class MapWidget extends StatefulWidget {
   final MapController controller;
   final void Function(dynamic)? onMessage;
   const MapWidget({super.key, required this.controller, this.onMessage});
 
   @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  late final WebViewController _webCtrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _webCtrl = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (_) {
+            widget.controller._init(_webCtrl);
+          },
+          onWebResourceError: (_) {},
+        ),
+      )
+      ..addJavaScriptChannel('MapMessage', onMessageReceived: (msg) {
+        try {
+          final data = jsonDecode(msg.message);
+          widget.onMessage?.call(data);
+        } catch (_) {}
+      });
+
+    _loadMapHtml();
+  }
+
+  Future<void> _loadMapHtml() async {
+    final htmlContent = await rootBundle.loadString('web/map.html');
+    await _webCtrl.loadHtmlString(htmlContent);
+  }
+
+  @override
   Widget build(BuildContext context) =>
-      const Center(child: Text('Map unavailable on this platform'));
+      WebViewWidget(controller: _webCtrl);
 }
