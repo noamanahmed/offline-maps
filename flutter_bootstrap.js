22b4f26a22439365fb438bba07c02144c025d0e8
noamanahmed <noamanahmed@users.noreply.github.com>
2026-07-02 11:45:58 +0000

    deploy: 928fd84ee7839f5f3a4e3ef04eb60497c8144db0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1143083767"
+    serviceWorkerVersion: "2713687883"
   }
 });
